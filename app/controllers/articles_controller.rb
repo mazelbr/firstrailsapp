7dc301017711418ae19a771bdc44e4913f71c5e4
hello <dunno>
2016-08-24 22:29:50 +0000

    extracted set_article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :destroy, :show]
+    
     
     def index
         @articles =Article.all
@@ -21,16 +23,16 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+        
     end
     
     
     def show
-        @article = Article.find(params[:id])
+        
     end
     
     def update
-        @article = Article.find(params[:id])
+       
         if 
             @article.update(article_params)
             flash[:notice] = "Succesfully updated"
@@ -42,13 +44,17 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         redirect_to articles_path
         flash[:notice] = "Article was succesfully deleted"
     end
     
     private
+        def set_article
+            @article = Article.find(params[:id])
+        end
+    
         def article_params
             params.require(:article).permit(:title, :description)
         end
