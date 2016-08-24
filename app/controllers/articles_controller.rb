f74487aad02e46be418bfba8e5b4261d12c7ccde
hello <dunno>
2016-08-24 21:57:18 +0000

    added edit, show, create, index listings, and linked the pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
     
+    def index
+        @articles =Article.all
+    end
+    
     def new
         @article = Article.new
     end
@@ -16,8 +20,25 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    
     def show
         @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if 
+            @article.update(article_params)
+            flash[:notice] = "Succesfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        
+        end
     end
     
     private
