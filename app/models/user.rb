3da45f296d6a2d41038cf8e2c99332fa5eddd418
hello <dunno>
2016-08-26 16:25:50 +0000

    setting up assosiations and debugger

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User <  ActiveRecord::Base
+    
+    has_many :articles
+    before_save { self.email =email.downcase } #turns email to downcase before saving
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3}
     VALID_EMAIL_REGEX= /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3}, format: {with: VALID_EMAIL_REGEX}
