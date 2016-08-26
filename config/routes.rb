22c828d964af6dfa21891e1186b52bab01c321f8
hello <dunno>
2016-08-26 17:48:44 +0000

    setting up sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 root 'pages#home'
 get 'about', to: 'pages#about'
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
+
 
 resources :articles
   # Example of regular route:
