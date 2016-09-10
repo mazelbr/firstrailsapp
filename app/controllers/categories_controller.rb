adc8be15996ea7b1fe7184e78da8e9cb0b44d1cc
hello <dunno>
2016-09-10 14:10:05 +0000

    deploy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,21 @@ class CategoriesController < ApplicationController
         @category = Category.find(params[:id])
         @category_articles = @category.articles.paginate(:page => params[:page], per_page: 5)
     end
+    
+    def edit
+        @category = Category.find(params[:id])
+    end
+    
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "Category succesfully updated"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
+        
+        end
+    end
 
     private
     def category_params
@@ -32,7 +47,7 @@ class CategoriesController < ApplicationController
     
     def require_admin
         if !logged_in? || (logged_in? and !current_user.admin?)
-            flash[:danger] = "You have to be Logged in"
+            flash[:danger] = "You have to be Logged in as an admin"
             redirect_to categories_path
         end
     
